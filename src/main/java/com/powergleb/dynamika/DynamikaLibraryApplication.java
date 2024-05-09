cfab4ec470abe80877a1a69bfcf0f064c890b147
g.rokah@yandex.ru <g.rokah@yandex.ru>
2024-05-09 18:28:00 +0300

    initial commit

---
+++ src/main/java/com/powergleb/dynamika/DynamikaLibraryApplication.java
@@ -0,0 +1,11 @@
+package com.powergleb.dynamika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamikaLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DynamikaLibraryApplication.class, args);
+    }
+}
